b87faf8ea6f80433533295f06381922369f9fd68
Ninjasri98 <palitsriparno@gmail.com>
2025-02-17 18:42:21 +0530

    added user booking service

--- app/src/main/java/org/example/services/UserBookingService.java
+++ app/src/main/java/org/example/services/UserBookingService.java
@@ -1,10 +1,22 @@
 package org.example.services;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
 import org.example.entities.User;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class UserBookingService {
     private User user;
-    public UserBookingService(User user){
+    private static final String USERS_PATH = "../localDb/users.json";
+    private ObjectMapper objectMapper = new ObjectMapper();
+    private List<User> userList;
+    public UserBookingService(User user) throws IOException{
         this.user = user;
+        File users = new File(USERS_PATH);
+        userList = objectMapper.readValue(users, new TypeReference<List<User>>() {});
     }
 }
