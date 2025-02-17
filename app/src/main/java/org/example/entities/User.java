23f4f2c9b23d56d1ae9f64fff02232a72a8b0d7b
Ninjasri98 <palitsriparno@gmail.com>
2025-02-17 21:08:20 +0530

    added getters and setters for user and train

--- app/src/main/java/org/example/entities/User.java
+++ app/src/main/java/org/example/entities/User.java
@@ -2,11 +2,54 @@ package org.example.entities;
 
 import java.util.List;
 
+import org.checkerframework.checker.units.qual.h;
+
 public class User {
     private String name;
     private String password;
     private String hashedPassword;
     private List<Ticket> ticketsBooked;
     private String userId;
-
+    public User(String name, String password, String hashedPassword , List<Ticket> ticketsBooked, String userId ){
+        this.name = name;
+        this.password = password;
+        this.ticketsBooked = ticketsBooked;
+        this.hashedPassword = hashedPassword;
+        this.userId = userId;
+    }
+    ///defualt constructor
+    public User(){}
+    ///getters
+    public String getName (){
+        return name;
+    }
+    public String getPassword (){
+        return password;
+    }
+    public String getHashedPassword (){
+        return hashedPassword;
+    }
+    public List<Ticket> getTicketsBooked (){
+        return ticketsBooked;
+    }
+    public String getUserId(){
+        return userId;
+    }
+    public void printTickets(){
+        for(int i = 0;i<ticketsBooked.size();i++){
+            System.out.println(ticketsBooked.get(i).getTicketInfo());
+        }
+    }
+    public void setName (String name){
+        this.name = name;
+    }
+    public void setHashedPassword(String hash){
+        this.hashedPassword = hash;
+    }
+    public void setTicketsBooked (List<Ticket> ticketsBooked){
+        this.ticketsBooked = ticketsBooked;
+    }
+    public void setUserId(String userId){
+        this.userId = userId;
+    }
 }
