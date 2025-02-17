ee5fd08c9e23569eddd96b4fdd44ab3ee0d76b31
Ninjasri98 <palitsriparno@gmail.com>
2025-02-17 12:00:06 +0530

    Added Services

--- app/src/main/java/org/example/entities/Ticket.java
+++ app/src/main/java/org/example/entities/Ticket.java
@@ -3,11 +3,11 @@ package org.example.entities;
 import java.sql.Date;
 
 public class Ticket {
-    private String TicketId;
-    private String UserId;
+    private String ticketId;
+    private String userId;
     private String source;
     private String destination;
-    private Date doj;
+    private Date dateOfTravel;
     private Train train;
-    
+
 }
--- app/src/main/java/org/example/entities/User.java
+++ app/src/main/java/org/example/entities/User.java
@@ -5,8 +5,8 @@ import java.util.List;
 public class User {
     private String name;
     private String password;
-    private String hashPassword;
+    private String hashedPassword;
     private List<Ticket> ticketsBooked;
     private String userId;
-    
+
 }
