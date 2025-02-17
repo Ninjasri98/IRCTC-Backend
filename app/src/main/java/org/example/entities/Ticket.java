dc39469d3813907c8db4fea771447fea35cef4ae
Ninjasri98 <palitsriparno@gmail.com>
2025-02-17 11:35:14 +0530

    Added Entities

---
+++ app/src/main/java/org/example/entities/Ticket.java
@@ -0,0 +1,13 @@
+package org.example.entities;
+
+import java.sql.Date;
+
+public class Ticket {
+    private String TicketId;
+    private String UserId;
+    private String source;
+    private String destination;
+    private Date doj;
+    private Train train;
+    
+}
