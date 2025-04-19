7557b700e9d09ad46f0abd2a7de39b2e77c228bc
Ninjasri98 <palitsriparno@gmail.com>
2025-04-19 19:48:38 +0530

    completed the app

--- app/src/main/java/org/example/util/UserServiceUtil.java
+++ app/src/main/java/org/example/util/UserServiceUtil.java
@@ -3,7 +3,7 @@ package org.example.util;
 import org.mindrot.jbcrypt.BCrypt;
 
 public class UserServiceUtil {
-    public static String getHashedPassword(String password) {
+    public static String hashPassword(String password) {
         return BCrypt.hashpw(password, BCrypt.gensalt());
     }
     public static Boolean checkPassword(String password, String hashedPassword) {
